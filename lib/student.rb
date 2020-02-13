c47e51c3e98b0dc9eda1e34ac82efa5f05acf73d
kowalix007 L.K <kowalix007@gmail.com>
2020-02-13 00:01:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,16 +5,7 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-      # student(student_hash)
-      # @name = name
-      # @location = location
-      # @twitter = twitter
-      # @linkedin = linkedin
-      # @github = github
-      # @blog = blog
-      # @profile_quote = profile_quote
-      # @bio = bio
-      # @profile_url = profile_url
+      @student_hash = student_hash
       @@all << self
   end
 
