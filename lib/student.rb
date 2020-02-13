9ef00a151c7d5ac986c639f2f2ac08529044db56
kowalix007 L.K <kowalix007@gmail.com>
2020-02-13 00:35:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,8 +5,8 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-      student_hash.each do |attribute, value|
-      self.send("#{attribute}=", value)
+      student_hash.each do |key, value|
+      self.send("#{key}=", value)
     end
       @@all << self
   end
