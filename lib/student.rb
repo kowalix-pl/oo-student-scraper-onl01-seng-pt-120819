ad252a831c8a3c5627f4535f4ca000b366d41d9b
kowalix007 L.K <kowalix007@gmail.com>
2020-02-13 00:37:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,8 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    students_array.each do |:name, :location|
+    students_array.each do |student_hash|
+      Student.new(name,location)
   end
 
   def add_student_attributes(attributes_hash)
